374066b0530002ff8a82fe1447936f24398613c8
chromigo <chromigo@gmail.com>
2017-07-08 15:03:28 +0500

    Add Visual Studio Extension project
    It give a button for converting views to *.cshtml

---
+++ VisualStudio.Extension/ProjectHelpers.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using EnvDTE;
+using EnvDTE80;
+
+namespace VisualStudio.Extension
+{
+    public static class ProjectHelpers
+    {
+        static DTE2 _dte = ConverToRazorCommand._dte;
+        public static string GetRootNamespace(this Project project)
+        {
+            if (project == null)
+                return null;
+
+            string ns = project.Name ?? string.Empty;
+
+            try
+            {
+                var prop = project.Properties.Item("RootNamespace");
+
+                if (prop != null && prop.Value != null && !string.IsNullOrEmpty(prop.Value.ToString()))
+                    ns = prop.Value.ToString();
+            }
+            catch { /* Project doesn't have a root namespace */ }
+
+            return CleanNameSpace(ns, stripPeriods: false);
+        }
+
+        public static string CleanNameSpace(string ns, bool stripPeriods = true)
+        {
+            if (stripPeriods)
+            {
+                ns = ns.Replace(".", "");
+            }
+
+            ns = ns.Replace(" ", "")
+                .Replace("-", "")
+                .Replace("\\", ".");
+
+            return ns;
+        }
+
+        public static string GetRootFolder(this Project project)
+        {
+            if (project == null)
+                return null;
+
+            if (project.IsKind(ProjectKinds.vsProjectKindSolutionFolder))
+                return Path.GetDirectoryName(_dte.Solution.FullName);
+
+            if (string.IsNullOrEmpty(project.FullName))
+                return null;
+
+            string fullPath;
+
+            try
+            {
+                fullPath = project.Properties.Item("FullPath").Value as string;
+            }
+            catch (ArgumentException)
+            {
+                try
+                {
+                    // MFC projects don't have FullPath, and there seems to be no way to query existence
+                    fullPath = project.Properties.Item("ProjectDirectory").Value as string;
+                }
+                catch (ArgumentException)
+                {
+                    // Installer projects have a ProjectPath.
+                    fullPath = project.Properties.Item("ProjectPath").Value as string;
+                }
+            }
+
+            if (string.IsNullOrEmpty(fullPath))
+                return File.Exists(project.FullName) ? Path.GetDirectoryName(project.FullName) : null;
+
+            if (Directory.Exists(fullPath))
+                return fullPath;
+
+            if (File.Exists(fullPath))
+                return Path.GetDirectoryName(fullPath);
+
+            return null;
+        }
+
+        public static ProjectItem AddFileToProject(this Project project, FileInfo file, string itemType = null)
+        {
+            var root = project.GetRootFolder();
+
+            if (string.IsNullOrEmpty(root) || !file.FullName.StartsWith(root, StringComparison.OrdinalIgnoreCase))
+                return null;
+
+            ProjectItem item = project.ProjectItems.AddFromFile(file.FullName);
+            item.SetItemType(itemType);
+            return item;
+        }
+
+        private static void SetItemType(this ProjectItem item, string itemType)
+        {
+            try
+            {
+                if (item?.ContainingProject == null)
+                    return;
+
+                if (string.IsNullOrEmpty(itemType))
+                    return;
+
+                item.Properties.Item("ItemType").Value = itemType;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex);
+            }
+        }
+
+        public static bool IsKind(this Project project, params string[] kindGuids)
+        {
+            foreach (var guid in kindGuids)
+            {
+                if (project.Kind.Equals(guid, StringComparison.OrdinalIgnoreCase))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ VisualStudio.Extension/WebFormsToRazorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Text;
+using Telerik.RazorConverter;
+using Telerik.RazorConverter.Razor.DOM;
+
+namespace VisualStudio.Extension
+{
+    public class WebFormsToRazorConverter
+    {
+        public WebFormsToRazorConverter()
+        {
+            var catalog = new AssemblyCatalog(typeof(IWebFormsParser).Assembly);
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+
+        [Import]
+        private IWebFormsParser Parser { get; set; }
+
+        [Import]
+        private IWebFormsConverter<IRazorNode> Converter { get; set; }
+
+        [Import]
+        private IRenderer<IRazorNode> Renderer { get; set; }
+
+        public string Convert(string filePath)
+        {
+            var webFormsPageSource = File.ReadAllText(filePath, Encoding.UTF8);
+            var webFormsDocument = Parser.Parse(webFormsPageSource);
+            var razorDom = Converter.Convert(webFormsDocument);
+            return Renderer.Render(razorDom);
+        }
+    }
+}
